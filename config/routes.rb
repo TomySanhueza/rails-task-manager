43373b026e6fda167a00ed5bd65ee897cdc74f14
TOMÁS IGNACIO. SANHUEZA <tomas.sanhuezaf@gmail.com>
2025-09-16 23:13:15 +0200

    Rails task manager, until 05

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get "tasks", to: "tasks#index", as: :tasks
+  get "tasks/new", to: "tasks#new", as: :new_task
+  get "task/:id", to: "tasks#show", as: :task
+  post "tasks", to: "tasks#create"
 end
